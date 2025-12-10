b3c311a3b2b4c158174e2c2df94c80cc6660be26
Kouta Sakatsume <kosakats@c3r2s1.42tokyo.jp>
2025-12-10 19:51:04 +0900

    PIPON_update

--- src/CMDS/handlePING.cpp
+++ src/CMDS/handlePING.cpp
@@ -4,10 +4,12 @@ void 	Server::handlePING(Client &c, const std::vector<std::string> &params)
 {
 	if (params.empty())
 	{
-    	c.sendMessage("409 PING :No origin specified\r\n"); // ERR_NEEDMOREPARAMS
+		// RFC 2812 specifies 461 for not enough parameters.
+    	c.sendMessage("461 PING :Not enough parameters\r\n");
     	return;
 	}
 	 // 引数1つ目だけ使用（2つ以上来ても無視）
 	const std::string &token = params[0];
-	c.sendMessage("PONG :" + token + "\r\n");
+	// The standard PONG reply includes the server name.
+	c.sendMessage("PONG " + _server_name + " :" + token + "\r\n");
 }
--- src/CMDS/handlePONG.cpp
+++ src/CMDS/handlePONG.cpp
@@ -1,9 +1,11 @@
 #include "../../include/Server.hpp"
+#include "../../include/Client.hpp"
 
 void Server::handlePONG(Client &c, const std::vector<std::string> &params)
 {
 	(void)params;
     // パラメータは特に使わない
+    // This correctly updates the client's last activity time.
     c.updatePongTime();
     
     // 任意: ログ表示
